20acc186128a1b1e9a9c343ca1cdb5052f55c863
zhangyan <1843447064@qq.com>
2019-04-29 11:52:03 +0800

    04-29

--- kmall/admin/src/pages/home/index.js
+++ kmall/admin/src/pages/home/index.js
@@ -1,20 +1,46 @@
 
 import React,{ Component } from 'react'
+import { connect } from 'react-redux'
+
+import { Card, Col, Row } from 'antd'
 
 import Layout from 'common/layout'
 
-import './index.css'
-
 class Home extends Component{
 	render(){
+		const {usernum,productnum,ordernum} = this.props
 		return (
 			<div className="Home">
 				<Layout>
-					home page!!!
+					<Row gutter={16}>
+						<Col span={8}>
+							<Card title="用户数量" bordered={false}>{usernum}</Card>
+						</Col>
+						<Col span={8}>
+							<Card title="商品数据" bordered={false}>{productnum}</Card>
+						</Col>
+						<Col span={8}>
+							<Card title="订单数量" bordered={false}>{ordernum}</Card>
+						</Col>
+				    </Row>
 				</Layout>
 			</div>
 		)
 	}
 }
 
-export default Home;
+const mapStateToProps = (state)=>{
+	return {
+		usernum:state.get('home').get('usernum'),
+		productnum:state.get('home').get('productnum'),
+		ordernum:state.get('home').get('ordernum')
+	}
+}
+
+const mapDispatchToProps = (dispatch)=>{
+	return {
+
+	}
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(Home);
